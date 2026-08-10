d532f9779c51f1b22f796ea46c4212e1fdf34e3f
kryptodel <danilo43278oskdj@gmail.com>
2026-08-10 10:00:00 -0300

    Modify XP gain and update player query with guild ID

--- commands/train.js
+++ commands/train.js
@@ -8,10 +8,11 @@ module.exports = {
 
   async execute(interaction) {
     const discordId = interaction.user.id;
+    const guildId = interaction.guild.id;
 
     const result = await db.query(
-      `SELECT level, xp, xp_to_next_level FROM players WHERE discord_id = $1`,
-      [discordId]
+      `SELECT level, xp, xp_to_next_level FROM players WHERE discord_id = $1 AND guild_id = $2`,
+      [discordId, guildId]
     );
 
     if (result.rows.length === 0) {
@@ -22,7 +23,7 @@ module.exports = {
     }
 
     const player = result.rows[0];
-    const xpGained = 25;
+    const xpGained = 15;
 
     let newXp = player.xp + xpGained;
     let newLevel = player.level;
@@ -32,20 +33,20 @@ module.exports = {
     if (newXp >= xpToNext) {
       newLevel += 1;
       newXp = newXp - xpToNext;
-      xpToNext = Math.floor(xpToNext * 1.5);
+      xpToNext = Math.floor(xpToNext * 1.6);
       leveledUp = true;
     }
 
     await db.query(
       `UPDATE players SET xp = $1, level = $2, xp_to_next_level = $3, max_level_reached = GREATEST(max_level_reached, $2)
-       WHERE discord_id = $4`,
-      [newXp, newLevel, xpToNext, discordId]
+       WHERE discord_id = $4 AND guild_id = $5`,
+      [newXp, newLevel, xpToNext, discordId, guildId]
     );
 
     if (leveledUp) {
       await interaction.reply(`You trained and gained **${xpGained} XP**!\nYou reached **level ${newLevel}**!`);
     } else {
-      await interaction.reply(`You trained and gained **${xpGained} XP**!\nCurrent XP: **${newXp}/${xpToNext}**`);
+      await interaction.reply(`You trained and gained **${xpGained} XP**!\nCurrent XP: ** ${newXp}/${xpToNext}**`);
     }
   }
 };
